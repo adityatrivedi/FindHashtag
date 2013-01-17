2a56cd07ac2be56f493aaff289ac88e4fee136b0
adityatrivedi <adityatriv@gmail.com>
2013-01-17 00:09:27 -0500

    remove add button from navigation bar

--- Twitter Search/MasterViewController.m
+++ Twitter Search/MasterViewController.m
@@ -29,9 +29,6 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.leftBarButtonItem = self.editButtonItem;
-
-    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
-    self.navigationItem.rightBarButtonItem = addButton;
     
     [self getTweets];
 }
